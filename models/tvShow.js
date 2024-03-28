209077859d3f46dfddc9891618b7d2950ef00441
NatNichols <natalie.e.nichols@gmail.com>
2024-03-28 17:51:06 -0400

    build TvShow model and tvEpisodeSchema embedded schema

---
+++ models/tvShow.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const tvEpisodeSchema = new Schema({
+  owner: {type: Schema.Types.ObjectId, ref: 'Profile'},
+  tmdbId: Number,
+  title: String,
+  airDate: Date,
+  season: Number,
+  episodeNum: Number,
+  description: String,
+  watchDate: Date,
+  watched: Number,
+})
+
+const tvShowSchema = new Schema({
+  owner: {type: Schema.Types.ObjectId, ref: 'Profile'},
+  tmdbId: Number,
+  title: String,
+  airDate: Date,
+  description: String,
+  watched: Number,
+  episodes: [tvEpisodeSchema],
+}, {
+  timestamps: true,
+})
+
+const TvShow = mongoose.model('TvShow', tvShowSchema)
+
+export { TvShow }
