72b047ab0011ffc741a5a0c4695fd70d2849c9fa
NatNichols <natalie.e.nichols@gmail.com>
2024-04-05 21:03:07 -0400

    remove dead code/comments

--- controllers/tvShows.js
+++ controllers/tvShows.js
@@ -5,7 +5,6 @@ const tmdbUrl = `https://api.themoviedb.org/3`
 
 export async function search(req, res) {
   try {
-    // console.log(`Mulder it's me`)
     console.log(`${tmdbUrl}/search/tv?query=${req.body.query}&include_adult=false&language=en-US?api_key=${process.env.TMDB_API_KEY}`)
     const config = {
       method: 'GET',
@@ -13,7 +12,6 @@ export async function search(req, res) {
         'Authorization': `Bearer ${process.env.TMDB_ACCESS_TOKEN}`,
         'Content-Type': 'application/json'
       },
-      // body: JSON.stringify(req.body.query)
     }
     const apiResponse = await fetch(`${tmdbUrl}/search/tv?query=${req.body.query}&include_adult=false&language=en-US?api_key=${process.env.TMDB_API_KEY}`, config)
     console.log(apiResponse)
@@ -24,8 +22,4 @@ export async function search(req, res) {
     res.status(500).json(err)
   }
 }
-
-
-//* https://api.themoviedb.org/3/search/tv?query=archer&include_adult=false&language=en-US?api_key=da963c06cd357e5d7efb870b363ed798 
-//! https://api.themoviedb.org/3/search/tv?query=archer&include_adult=false&language=en-US?api_key=da963c06cd357e5d7efb870b363ed798
 
